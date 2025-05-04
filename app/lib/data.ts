dd8eb57a960973839716a10652f57c88ab1658be
Boopkin <boopkin@furnavia.org>
2025-05-04 15:18:10 +0200

    update through chapter 12

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,12 +1,5 @@
 import postgres from 'postgres';
-import {
-    CustomerField,
-    CustomersTableType,
-    InvoiceForm,
-    InvoicesTable,
-    LatestInvoiceRaw,
-    Revenue,
-} from './definitions';
+import {CustomerField, CustomersTableType, InvoiceForm, InvoicesTable, LatestInvoiceRaw, Revenue,} from './definitions';
 import {formatCurrency} from './utils';
 
 const sql = postgres(process.env.POSTGRES_URL!, {
@@ -136,8 +129,7 @@ export async function fetchInvoicesPages(query: string) {
                                   OR invoices.status ILIKE ${`%${query}%`}
         `;
 
-        const totalPages = Math.ceil(Number(data[0].count) / ITEMS_PER_PAGE);
-        return totalPages;
+        return Math.ceil(Number(data[0].count) / ITEMS_PER_PAGE);
     } catch (error) {
         console.error('Database Error:', error);
         throw new Error('Failed to fetch total number of invoices.');
--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -21,9 +21,7 @@ export default function Pagination({totalPages}: { totalPages: number }) {
 
     return (
         <>
-            {/*  NOTE: Uncomment this code in Chapter 11 */}
-
-            {/* <div className="inline-flex">
+            {<div className="inline-flex">
         <PaginationArrow
           direction="left"
           href={createPageURL(currentPage - 1)}
@@ -56,7 +54,7 @@ export default function Pagination({totalPages}: { totalPages: number }) {
           href={createPageURL(currentPage + 1)}
           isDisabled={currentPage >= totalPages}
         />
-      </div> */}
+      </div>}
         </>
     );
 }
